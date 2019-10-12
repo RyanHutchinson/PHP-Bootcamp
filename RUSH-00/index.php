fb80f2bbc6720a654c41fd7b5765b25fca6a1ec2
Ryan HUTCHINSON <rhutchin@student.wethinkcode.co.za>
2019-10-12 14:22:41 +0200

    1st push

---
+++ RUSH-00/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="./Resources/css/style.css">
+</head>
+<body>
+
+<div class="navbar">
+  <a href="./index.php">Home</a>
+  <div class="dropdown">
+    <button class="dropbtn"><a class="store" href="./pages/store-catagories.php"> Cheese Store </a>
+      <i class="" href="http://www.google.com"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="./pages/stinky-cheese.php">Stinky Cheese</a>
+      <a href="./pages/processed-cheese.php">Processed Cheese</a>
+      <a href="./pages/weird-cheese.php">Weird Cheese</a>
+    </div>
+  </div>
+  <a href="./pages/about-us.php">About Us</a>
+  <a href="./pages/contact-us.php">Contact Us</a>
+</div>
+<div class="mainbody">
+  <p>Welcome to cheeseland!</p>
+</div>
+
+</body>
+</html>
---
+++ RUSH-00/pages/about-us.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../Resources/css/style.css">
+</head>
+<body>
+
+<div class="navbar">
+  <a href="../index.php">Home</a>
+  <div class="dropdown">
+    <button class="dropbtn"><a class="store" href="store-catagories.php"> Cheese Store </a>
+      <i class="" href="http://www.google.com"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="stinky-cheese.php">Stinky Cheese</a>
+      <a href="processed-cheese.php">Processed Cheese</a>
+      <a href="weird-cheese.php">Weird Cheese</a>
+    </div>
+  </div>
+  <a href="about-us.php">About Us</a>
+  <a href="contact-us.php">Contact Us</a>
+</div>
+
+<h3>To-do</h3>
+
+</body>
+</html>
